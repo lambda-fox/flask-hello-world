885cfd46ba0c4457236e6464ff08644f8e308420
lambda-fox <pallavk@outlook.com>
2014-09-02 13:09:09 +0800

    Learning about dynamic routes and turn on DEBUG mode

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from flask import Flask
 # create the application object
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 # use decorators to link the function to a url
 @app.route("/")
@@ -11,7 +12,24 @@ app = Flask(__name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!...!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def type(value):
+	print value+1
+	return "Correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello {}".format(name), 200
+	else:
+		return "Name not found", 404
 
 # start the development server using the run() method
 if __name__ == "__main__":
